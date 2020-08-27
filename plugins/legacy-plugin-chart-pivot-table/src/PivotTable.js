4837bfa3920e1ead31aca069b1cc935bc239275a
Camilo <camilo.a@uptimeanalytics.com>
2020-08-27 12:32:58 -0500

    295fb0c8ae157ba2c956b5fc1dd9f970a1981b64 and e6568fe7f9c09268f4df9a8276ca1fe027ff6920

--- plugins/legacy-plugin-chart-pivot-table/src/PivotTable.js
+++ plugins/legacy-plugin-chart-pivot-table/src/PivotTable.js
@@ -91,7 +91,6 @@ function PivotTable(element, props) {
       bInfo: false,
       scrollY: `${height}px`,
       scrollCollapse: true,
-      scrollX: true,
     });
     table.column('-1').order('desc').draw();
     fixTableHeight($container.find('.dataTables_wrapper'), height);
